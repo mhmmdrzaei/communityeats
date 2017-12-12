570b3109962f71758f5c6ae0f8defc4e431b46cb
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2017-12-12 17:09:16 -0500

    bare stylings for project page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,3 +13,14 @@ $('label').on('click', function (event) {
     // event.preventDefault();
     $('nav menu').toggleClass('away');
 });
+
+//span text menu
+
+var tooltipSpan = document.getElementById('tooltip-span');
+
+window.onmousemove = function (e) {
+    var x = e.clientX,
+        y = e.clientY;
+    tooltipSpan.style.top = y + 20 + 'px';
+    tooltipSpan.style.left = x + 20 + 'px';
+};
